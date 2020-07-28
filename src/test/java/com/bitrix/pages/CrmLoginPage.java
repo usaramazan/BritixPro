07680476fe429e1aeebfb35492c1f46a2f5da98e
svgnhkmglu <sevginhekimoglu@gmail.com>
2020-07-28 00:33:01 -0400

    BtrixMassage

---
+++ src/test/java/com/bitrix/pages/CrmLoginPage.java
@@ -0,0 +1,26 @@
+package com.bitrix.pages;
+
+import com.bitrix.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CrmLoginPage {
+
+
+    public CrmLoginPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+    @FindBy(xpath = "//input[@name='USER_LOGIN']")
+    public WebElement usernameInput;
+
+    @FindBy(xpath = "//input[@type='password']")
+    public WebElement passwordInput;
+
+    @FindBy(xpath = "//input[@type='submit']")
+    public WebElement loginButton;
+
+
+}
