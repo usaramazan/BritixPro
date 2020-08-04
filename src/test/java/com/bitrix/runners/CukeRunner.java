bd59c21afc26207ddbd7cabdd9377a64664d4d94
Marina Velitskaia <marinavelitskaia@gmail.com>
2020-08-03 23:10:32 -0400

    jen

--- src/test/java/com/bitrix/runners/CukeRunner.java
+++ src/test/java/com/bitrix/runners/CukeRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/crm24/stepdefinitions",
         dryRun = false,
-        tags = "@smoke"
+        tags = "@login"
 )
 
 public class CukeRunner {
